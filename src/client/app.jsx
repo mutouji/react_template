2a0894ec896186f06150ce77dfa163f6776071fd
mutouji <59022665@qq.com>
2018-01-28 15:04:28 +0800

    add redux-thunk for action creator return a function not an object

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -3,7 +3,9 @@
 
 import React from 'react'
 import HelloButton from './container/hello-button'
+import HelloAsyncButton from './container/hello-async-button'
 import Message from './container/message'
+import MessageAsync from './container/message-async'
 import { APP_NAME } from '../shared/config'
 
 const App = () =>
@@ -11,6 +13,8 @@ const App = () =>
     <h1>{APP_NAME}</h1>
     <Message />
     <HelloButton />
+    <MessageAsync />
+    <HelloAsyncButton />
    </div>)
 
 export default App
